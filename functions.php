bdf7c870212ef5b0f422d589af88061b466401c3
trendywebworks <trendywebworks@gmail.com>
2026-07-18 13:38:52 +0530

    updated

--- functions.php
+++ functions.php
@@ -12,18 +12,7 @@ if ( ! function_exists( 'wpt_setup' ) ):
         register_nav_menu( 'footer_nav_4', __( 'Footer Navigation 4', 'wptuts' ) );
         register_nav_menu( 'footer_nav_5', __( 'Footer Navigation 5', 'wptuts' ) );
 	} endif;
-// Register bootstrap files and jQuery:
-function wpt_register_js() {
-    //wp_register_script('jquery.bootstrap.min', get_template_directory_uri() . '/js/bootstrap.min.js', 'jquery');
-    //wp_enqueue_script('jquery.bootstrap.min');
-}
-add_action( 'init', 'wpt_register_js' );
-function wpt_register_css() {
-    //wp_register_style( 'bootstrap.min', get_template_directory_uri() . '/css/bootstrap.min.css' );
-    //wp_enqueue_style( 'bootstrap.min' );
-}
-add_action( 'wp_enqueue_scripts', 'wpt_register_css' );
-require_once('wp_bootstrap_navwalker.php');
+
 
 /* GET ACTIVE CLASS */
 
